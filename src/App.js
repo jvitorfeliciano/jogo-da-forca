a4ab2a96f156a0c2fe151c1d4245e48ccd590cf9
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-09-21 18:45:49 -0300

    mudança de imagem após erro

--- src/App.js
+++ src/App.js
@@ -1,37 +1,67 @@
 import palavras from "./palavras"
+import React from "react"
 
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function Keyboard(props){
-    return(
-        <button type="button" value={props.value}>
-            {props.value.toUpperCase()}
-        </button>
-    )
-}
+const imgArchive= ['./assets/forca0.png','./assets/forca1.png','./assets/forca2.png','./assets/forca3.png','./assets/forca4.png','./assets/forca5.png','./assets/forca6.png'];
+
+let number;
+let wordVector;
+let letterReceived;
+let counterMistakes=0;
 
 export default function App(){
+    
+    const [displayWord, setDisplayWord] = React.useState('');
+    const [imgHanged, setImgHanged] = React.useState(imgArchive[counterMistakes]);
+
+    function chooseWord(){
+        number = Math.floor(Math.random()*palavras.length) ;
+        wordVector = palavras[number].split('');
+        setDisplayWord(wordVector.map((p) => ' _'));
+        console.log(wordVector)    
+    }
+    
+    function getLetter(e){
+        // condição para resolver o bug da imagem sumir, quando contador de erro for igual a 6 a função retorna e nada mais é executado dentro dela;
+        if(counterMistakes >= 6){ 
+            return
+        }
+        letterReceived = e.target.value
+
+        if (wordVector!==undefined){
+            if(wordVector.includes(letterReceived)){
+                alert('Simboraaaa')
+            }
+            else{
+                counterMistakes++
+                setImgHanged(imgArchive[counterMistakes])
+            }   
+        }           
+    }
     return(
         <main>
             <section className='hangman'>
                 <figure>
-                    <img src='./assets/forca0.png'/>
+                    <img src={imgHanged}/>
                 </figure>
-                <button type='button'>
+                <button type='button' onClick={chooseWord}>
                     Escolher Palavra
                 </button>
                 <div className='word'>
-                   Aqui vai a palavra
+                  {displayWord}
                 </div>  
             </section>
             <section className='keyboard'>
-                {alfabeto.map((k,index) =><Keyboard key={index} value={k}/>)}
+                {alfabeto.map((k,index) => <button key={index}type="button" value={k} onClick={(e)=> getLetter(e)} >
+                {k.toUpperCase()}
+                    </button>)}
             </section>
             <section className="guesswork">
                 <form>
                     <label>Já sei a palavra!</label>
                     <input type="text"/>
-                    <button>
+                    <button type="button">
                         Chutar
                     </button>
                 </form>
@@ -39,3 +69,4 @@ export default function App(){
         </main>
     )
 }
+
